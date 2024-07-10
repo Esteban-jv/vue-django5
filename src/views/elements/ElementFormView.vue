e0e6a18577653f66480f71056f0b0c44ffd343d6
Esteban-jv <ejv.developer@gmail.com>
2024-07-10 12:52:04 -0500

    Decent text editor

--- src/views/elements/ElementFormView.vue
+++ src/views/elements/ElementFormView.vue
@@ -1,7 +1,13 @@
 <script setup>
     import { ref, inject, onMounted, computed } from 'vue'
     import { useRoute, useRouter } from 'vue-router'
-    import { ClassicEditor, Bold, Essentials, Italic, Mention, Paragraph, Undo, List, FindAndReplace, Font } from 'ckeditor5';
+    import { ClassicEditor, Bold, Essentials, Italic, Mention, Paragraph, Undo, List, FindAndReplace, Font,
+        Alignment, Indent, IndentBlock, Underline,Heading, Table,
+        TableCaption, TableCellProperties, TableColumnResize, TableProperties, TableToolbar,
+        ImageInsert, ImageBlock, ImageCaption, ImageInline, ImageInsertViaUrl, ImageResize, ImageStyle, ImageToolbar,
+        Minimap
+
+     } from 'ckeditor5';
     import 'ckeditor5/ckeditor5.css';
 
     const axios = inject('axios')
@@ -29,18 +35,103 @@
     const categories = ref([])
     const types = ref([])
     const editor = ref(ClassicEditor)
+    const editorMinimapElement = ref(null)
     const editorConfig = ref({
-              plugins: [ Bold, Essentials, Italic, Mention, Paragraph, Undo, List, FindAndReplace, Font ],
-              toolbar: [ 'undo', 'redo', 'findandreplace', '|', 
-                    'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor', '|',
-                    'pharagraph', 'bold', 'italic', '|',
-                    'bulletedList', 'numberedList'
-                ],
-              licenseKey: '<YOUR_LICENSE_KEY>',
-              mention: { 
-                  // Mention configuration
-              },
-            })
+        plugins: [ Bold, Essentials, Italic, Mention, Paragraph, Undo, List, FindAndReplace, Font,
+            Alignment, Indent, IndentBlock, Underline, Heading,
+            Table, TableCaption, TableCellProperties, TableColumnResize, TableProperties, TableToolbar,
+            ImageInsert, ImageInsertViaUrl, ImageBlock, ImageCaption, ImageInline, ImageInsertViaUrl, ImageResize, ImageStyle, ImageToolbar,
+            Minimap
+        ],
+        toolbar: [ 'undo', 'redo', 'findandreplace', '|', 
+            'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor', '|',
+            'bold', 'italic', 'underline', '|',
+            'alignment',
+            '|',
+            'bulletedList',
+            'numberedList',
+            'indent',
+            'outdent',
+            '|',
+            'insertTable',
+            'toggleImageCaption',
+            'insertImage',
+            'insertImageViaUrl',
+            'imageStyle:inline',
+            'imageStyle:wrapText',
+            'imageStyle:breakText',
+            'resizeImage'
+        ],
+        fontFamily: {
+            supportAllValues: true
+        },
+        fontSize: {
+            options: [10, 12, 14, 'default', 18, 20, 22],
+            supportAllValues: true
+        },
+        heading: {
+            options: [
+                {
+                    model: 'paragraph',
+                    title: 'Paragraph',
+                    class: 'ck-heading_paragraph'
+                },
+                {
+                    model: 'heading1',
+                    view: 'h1',
+                    title: 'Heading 1',
+                    class: 'ck-heading_heading1'
+                },
+                {
+                    model: 'heading2',
+                    view: 'h2',
+                    title: 'Heading 2',
+                    class: 'ck-heading_heading2'
+                },
+                {
+                    model: 'heading3',
+                    view: 'h3',
+                    title: 'Heading 3',
+                    class: 'ck-heading_heading3'
+                },
+                {
+                    model: 'heading4',
+                    view: 'h4',
+                    title: 'Heading 4',
+                    class: 'ck-heading_heading4'
+                },
+                {
+                    model: 'heading5',
+                    view: 'h5',
+                    title: 'Heading 5',
+                    class: 'ck-heading_heading5'
+                },
+                {
+                    model: 'heading6',
+                    view: 'h6',
+                    title: 'Heading 6',
+                    class: 'ck-heading_heading6'
+                }
+            ]
+        },
+        list: {
+            properties: {
+                styles: true,
+                startIndex: true,
+                reversed: true
+            }
+        },
+        table: {
+            contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
+        },
+        image: {
+            toolbar: ['imageTextAlternative', '|', 'imageStyle:inline', 'imageStyle:wrapText', 'imageStyle:breakText', '|', 'resizeImage']
+        },
+        minimap: {
+            container: editorMinimapElement,
+            extraClasses: 'editor-container_include-minimap ck-minimap__iframe-content'
+        },
+    })
 
     // MOUNTED
     onMounted( async () => {
@@ -143,7 +234,40 @@
                     }"
                 />
             </NFormItem>
-            <Ckeditor :editor="editor" v-model="form.description" :config="editorConfig"></Ckeditor>
+            <!-- <Ckeditor :editor="editor" v-model="form.description" :config="editorConfig"></Ckeditor> -->
+            <!-- <div class="main-container">
+                <div class="editor-container editor-container_document-editor editor-container_include-annotations" ref="editorContainerElement">
+                    <div class="editor-container__menu-bar" ref="editorMenuBarElement"></div>
+                    <div class="editor-container__toolbar" ref="editorToolbarElement"></div>
+                    <div class="editor-container__editor-wrapper">
+                        <div class="editor-container__editor">
+                            <div ref="editorElement">
+                                <Ckeditor :editor="editor" v-model="form.description" :config="editorConfig" @ready="onReady"></Ckeditor>
+                            </div>
+                        </div>
+                        <div class="editor-container__sidebar editor-container__sidebar_minimap"><div ref="editorMinimapElement"></div></div>
+                    </div>
+                </div>
+            </div> -->
+            <div class="main-container">
+			<div
+				class="editor-container editor-container_document-editor editor-container_include-minimap editor-container_include-style"
+				ref="editorContainerElement"
+			>
+				<div class="editor-container__menu-bar" ref="editorMenuBarElement"></div>
+                    <div class="editor-container__toolbar" ref="editorToolbarElement"></div>
+                    <div class="editor-container__minimap-wrapper">
+                        <div class="editor-container__editor-wrapper">
+                            <div class="editor-container__editor">
+                                <div ref="editorElement">
+                                    <Ckeditor :editor="editor" v-model="form.description" :config="editorConfig"></Ckeditor>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="editor-container__sidebar editor-container__sidebar_minimap"><div ref="editorMinimapElement"></div></div>
+                    </div>
+                </div>
+            </div>
             <NFormItem label="Category" path="selectValue" :feedback="getFeedback('category')">
                 <NSelect
                     :status="getStatus('category')"
